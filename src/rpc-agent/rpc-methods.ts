5149312cb7992ff335bd3f178372b33822f02251
Toolkit Dev <toolkit@local>
2026-04-20 20:18:35 +0200

    refactor(rpc): introduce index + aggregator, retire old 99-rpc-agent

---
+++ src/rpc-agent/rpc-methods.ts
@@ -0,0 +1,17 @@
+// Aggregates all rpc-agent modules into a single flat export namespace.
+// Used by index.ts as the value of rpc.exports.
+import * as searchRpc from "./search";
+import * as explorerRpc from "./explorer";
+import * as hooksRpc from "./hooks";
+import * as instanceOpsRpc from "./instance-ops";
+import * as networkRpc from "./network";
+
+export function getRpcMethods(): typeof searchRpc & typeof explorerRpc & typeof hooksRpc & typeof instanceOpsRpc & typeof networkRpc {
+    return {
+        ...searchRpc,
+        ...explorerRpc,
+        ...hooksRpc,
+        ...instanceOpsRpc,
+        ...networkRpc,
+    } as typeof searchRpc & typeof explorerRpc & typeof hooksRpc & typeof instanceOpsRpc & typeof networkRpc;
+}
